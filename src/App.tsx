d1c1f6b6cfd460d6af5ef54fd96bd246f3b14fd9
p0lluxstar <web-registra@yandex.ru>
2025-01-25 15:43:08 +0300

    refac: comp Header, App; T: 25-01-2025 15:43

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import 'normalize.css';
 import './styles/global.scss';
-import { useTranslation } from 'react-i18next';
 import Footer from './components/Footer';
 import Header from './components/Header/index';
 import Main from './components/Main';
 import Scroll from './components/Scroll';
-import i18n from './i18n';
+
 import 'font-awesome/css/font-awesome.min.css';
 
 function App(): JSX.Element {
-  const { t } = useTranslation();
-
-  const changeLanguage = (lng: string): void => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
     <>
-      <div>
-        <label>{t('language')}: </label>
-        <button onClick={() => changeLanguage('en')}>English</button>
-        <button onClick={() => changeLanguage('ru')}>Русский</button>
-      </div>
       <Header />
       <Main />
       <Footer />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import { URL_GITHUB, NAV_LIST_ITEM } from '../../constants/components';
+import i18n from '../../i18n';
 import styles from './index.module.scss';
 
 export default function Header(): JSX.Element {
   const { t } = useTranslation();
+
+  const toggleLanguage = (): void => {
+    const currentLanguage = i18n.language;
+    const newLanguage = currentLanguage === 'en' ? 'ru' : 'en';
+    i18n.changeLanguage(newLanguage);
+  };
 
   return (
     <header className={`${styles.header} ${styles.center}`}>
@@ -25,20 +32,30 @@ export default function Header(): JSX.Element {
             </li>
           ))}
         </ul>
-        {/* <button
-          type="button"
-          aria-label="toggle theme"
-          className="btn btn--icon"
-        >
-          <i aria-hidden="true" id="btn-theme" className="fas fa-moon"></i>
-        </button>
-        <button
-          type="button"
-          aria-label="toggle navigation"
-          className="btn btn--icon nav__hamburger"
-        >
-          <i aria-hidden="true" className="fas fa-bars"></i>
-        </button> */}
+        <div className={styles.btnIconWrapper}>
+          <button
+            type="button"
+            aria-label="toggle theme"
+            className={`${styles.btn} ${styles.btnIcon}`}
+          >
+            <i aria-hidden="true" id="btn-theme" className="fa fa-moon-o"></i>
+          </button>
+          <button
+            type="button"
+            aria-label="toggle lang"
+            className={`${styles.btn} ${styles.btnIcon}`}
+            onClick={toggleLanguage}
+          >
+            <i className="fa fa-globe" aria-hidden="true"></i>
+          </button>
+          <button
+            type="button"
+            aria-label="toggle navigation"
+            className={`${styles.btn} ${styles.btnIcon} ${styles.navHamburger}`}
+          >
+            <i aria-hidden="true" className="fa fa-bars"></i>
+          </button>
+        </div>
       </nav>
     </header>
   );
